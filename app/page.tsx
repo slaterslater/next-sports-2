c417af25c122c82cdb192cb58f93a21e6f1da45a
slaterslater <slaterslater@gmail.com>
2024-08-27 23:40:12 -0400

    scrapes multiple pages and returns single data array

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,24 @@
 import * as cheerio from 'cheerio'
 
 export const revalidate = 60 // invalidate every minute
+const PAGES = 3
 
 export default async function Home() {
 
-  const URL = 'https://www.nbcsports.com/fantasy/football/player-news?f0=Headline&p=1'
-  const resp = await fetch(URL)
-  const text = await resp.text()
-  const $ = cheerio.load(text)
+  const headlines = [] as string[]
 
-  const names = $('.PlayerNewsPost-name').map(function() {
-    return $(this).text();
-    }).toArray();
+  for (let i = 1; i <= PAGES; i++){
+    const URL = `https://www.nbcsports.com/fantasy/football/player-news?f0=Headline&p=${i}`
+    const resp = await fetch(URL)
+    const text = await resp.text()
+    const $ = cheerio.load(text)
+
+    const news = $('.PlayerNewsPost-name').map(function() {
+      return $(this).text();
+      }).toArray();
+
+    headlines.push(...news)
+  }
 
   let date = new Date().toDateString()
   
@@ -19,7 +26,7 @@ export default async function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
       <ul>
-        {names.map((name, i) => <li key={i}>{name}</li>)}
+        {headlines.map((headline, i) => <li key={i}>{headline}</li>)}
       </ul>
       </div>
       <div>{date}</div>
